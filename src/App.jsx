4fa8e69b81571220ac6bdfbdfc1fd27dcd8fc48e
princekafle <89830489+princekafle@users.noreply.github.com>
2025-04-18 20:33:05 +0545

    final

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import Title from "./components/Title";
+import TodoItem from "./components/TodoItem";
+import AddTodo from "./components/AddTodo";
+
+function App() {
+  // const [stateVariableName, functionToUpdateThisStateVar] = useState(InitalValue)
+  const [todos, setTodos] = useState(() => {
+    const storedTodos = localStorage.getItem("todos");
+    return storedTodos ? JSON.parse(storedTodos) : []; // stored data xa vane parse garera return garne, stored data chaina vane empty array return garne
+
+  });
+
+  function addTodo(task) {
+    setTodos([task, ...todos]);
+  }
+
+  function deleteTodo(id) {
+    const remainingTodos = todos.filter((todo) => todo.id != id);
+
+    setTodos(remainingTodos);
+  }
+
+  function updateTodo(id, data) {
+    const updatedTodos = todos.map((todo) => (todo.id === id ? data : todo));
+
+    setTodos(updatedTodos);
+  }
+
+  useEffect(() => {
+    console.log(todos);
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <main className="py-20 px-8">
+      <div className="container w-full md:w-2/3 lg:w-1/2 mx-auto py-5 px-6 md:py-8 md:px-12 bg-slate-100 shadow-lg rounded-2xl">
+        <Title />
+        <AddTodo addTodo={addTodo} />
+
+        <div>
+          {todos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              deleteTodo={deleteTodo}
+              updateTodo={updateTodo}
+            />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react"
+
+function TodoItem({todo, deleteTodo, updateTodo}) {
+    const [text, setText] = useState(todo.name);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function saveEdit(){
+        updateTodo(todo.id, {...todo, name: text.trim()});
+        setIsEditing(false);
+    }
+  return (
+    <div
+      className={`bg-white my-2 rounded-lg px-4 py-2 flex items-center justify-between gap-3 ${
+        todo.isComplete ? "opacity-50" : ""
+      }`}
+    >
+      <input
+        type="checkbox"
+        checked={todo.isComplete} // if todo is complete, check the box
+        className="cursor-pointer"
+        onChange={() =>
+          updateTodo(todo.id, { ...todo, isComplete: !todo.isComplete }) // If it was true, it becomes false. If it was false, it becomes true.
+        }
+      />
+      <input
+        type="text"
+        value={text}
+        disabled={!isEditing} // if not editing, disable the input
+        className={`${todo.isComplete ? "line-through" : ""} ${
+          isEditing ? "border-2" : ""
+        } w-full px-2 border-slate-300 rounded`}
+        onKeyDown={(e) => e.key == "Enter" && saveEdit()} // if enter is pressed, save the edit
+        onChange={(e) => setText(e.target.value)}
+      />
+
+      <button
+        className="bg-blue-100 px-2 py-1 shadow hover:bg-blue-200 rounded"
+        onClick={() => setIsEditing(true)}
+      >
+        ✏️
+      </button>
+
+      <button
+        className="bg-red-100 px-2 py-1 shadow hover:bg-red-200 rounded"
+        onClick={() => deleteTodo(todo.id)}
+      >
+        🗑️
+      </button>
+    </div>
+  )
+}
+
+export default TodoItem
